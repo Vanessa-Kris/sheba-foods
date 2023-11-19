38b63a69231fbc0d7655f2bc565c74fcb040e580
Minion <nessakristo@gmail.com>
2023-11-19 19:38:35 +0100

    made hero section responsive

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,169 +1,242 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-import { Box, Typography, IconButton } from "@mui/material";
-
-const renderArrowPrev = (clickHandler, hasPrev, label) => (
-  <IconButton
-    onClick={clickHandler}
-    disabled={!hasPrev}
-    sx={{
-      position: "absolute",
-      top: { md: "47%", xs: "38%" },
-      left: { md: "40px", xs: 0 },
-      zIndex: 2,
-    }}
-  >
-    <Box
-      component="img"
-      src="/doublewheat.png"
-      alt="leaf"
-      sx={{ width: "60% !important", rotate: "-100deg" }}
-    />
-  </IconButton>
-);
-
-const renderArrowNext = (clickHandler, hasNext, label) => (
-  <IconButton
-    onClick={clickHandler}
-    disabled={!hasNext}
-    sx={{
-      position: "absolute",
-      top: { md: "47%", xs: "38%" },
-      right: { md: "47px", xs: 0 },
-      zIndex: 2,
-    }}
-  >
-    <Box
-      component="img"
-      src="/doublewheat.png"
-      alt="leaf"
-      sx={{
-        width: "60% !important",
-        rotate: "80deg",
-      }}
-    />
-  </IconButton>
-);
+import { Box, Typography } from "@mui/material";
 
 export default function BannerCarousel() {
-  const isMobileView = window.innerWidth <= 600; // Adjust the breakpoint as needed
   return (
-    <Carousel
-      autoPlay
-      renderArrowPrev={isMobileView ? undefined : renderArrowPrev}
-      renderArrowNext={isMobileView ? undefined : renderArrowNext}
-    >
-      <Box component="div" className="banner">
-        <Typography
-          variant="h2"
-          textAlign="center"
-          sx={{
-            fontWeight: 700,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            textAlign: "center",
-            height: "100%",
-            // zIndex: 1,
-            color: "#fff",
-            fontSize: { md: 80, sm: 60, xs: 55 },
-          }}
-        >
-          <span
-            style={{
-              color: "#698F3C",
-              fontFamily: "monospace",
-            }}
-          >
-            Healthy
-          </span>{" "}
-          & Tasty food Choice
-        </Typography>
+    <>
+      <Box sx={{ display: { md: "block", sm: "block", xs: "none" } }}>
+        <Carousel infiniteLoop autoPlay>
+          <Box component="div" className="banner">
+            <Typography
+              variant="h2"
+              textAlign="center"
+              sx={{
+                fontWeight: 700,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                height: "100%",
+                // zIndex: 1,
+                color: "#fff",
+                fontSize: { md: 80, sm: 60, xs: 55 },
+              }}
+            >
+              <span
+                style={{
+                  color: "#698F3C",
+                  fontFamily: "monospace",
+                }}
+              >
+                Healthy
+              </span>{" "}
+              & Tasty food Choice
+            </Typography>
+          </Box>
+          <Box component="div" className="banner1">
+            <Typography
+              variant="h2"
+              textAlign="center"
+              sx={{
+                fontWeight: 700,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                height: "100%",
+                color: "#fff",
+                fontSize: { md: 80, sm: 60, xs: 55 },
+              }}
+            >
+              Amazing
+              <span
+                style={{
+                  color: "#698F3C",
+                  fontFamily: "monospace",
+                }}
+              >
+                Natural
+              </span>
+              recipes for you to try!
+            </Typography>
+          </Box>
+          <Box component="div" className="banner2">
+            <Typography
+              variant="h2"
+              textAlign="center"
+              sx={{
+                fontWeight: 700,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                height: "100%",
+                color: "#fff",
+                fontSize: { md: 80, sm: 60, xs: 55 },
+              }}
+            >
+              Perfect meal for the whole{" "}
+              <span
+                style={{
+                  color: "#83B841",
+                  fontFamily: "monospace",
+                }}
+              >
+                Family
+              </span>
+            </Typography>
+          </Box>
+          <Box component="div" className="banner3">
+            <Typography
+              variant="h2"
+              textAlign="center"
+              sx={{
+                fontWeight: 700,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                height: "100%",
+                color: "#fff",
+                fontSize: { md: 80, sm: 60, xs: 55 },
+              }}
+            >
+              Freshly sourced from local{" "}
+              <span
+                style={{
+                  color: "#698F3C",
+                  fontFamily: "monospace",
+                }}
+              >
+                Farmers
+              </span>
+            </Typography>
+          </Box>
+        </Carousel>
       </Box>
-      <Box component="div" className="banner1">
-        <Typography
-          variant="h2"
-          textAlign="center"
-          sx={{
-            fontWeight: 700,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            textAlign: "center",
-            height: "100%",
-            color: "#fff",
-            fontSize: { md: 80, sm: 60, xs: 55 },
-          }}
-        >
-          Amazing
-          <span
-            style={{
-              color: "#698F3C",
-              fontFamily: "monospace",
-            }}
-          >
-            Natural
-          </span>
-          recipes for you to try!
-        </Typography>
+      {/* Mobile */}
+      <Box sx={{ display: { md: "none", sm: "none", xs: "block" } }}>
+        <Carousel infiniteLoop autoPlay>
+          <Box component="div" className="bannermobile">
+            <Typography
+              variant="h2"
+              textAlign="center"
+              sx={{
+                fontWeight: 700,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                height: "100%",
+                color: "#fff",
+                fontSize: 40,
+              }}
+            >
+              <span
+                style={{
+                  color: "#698F3C",
+                  fontFamily: "monospace",
+                }}
+              >
+                Healthy
+              </span>{" "}
+              & Tasty food Choice
+            </Typography>
+          </Box>
+          <Box component="div" className="banner1mobile">
+            <Typography
+              variant="h2"
+              textAlign="center"
+              sx={{
+                fontWeight: 700,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                height: "100%",
+                color: "#fff",
+                fontSize: 40,
+              }}
+            >
+              Perfect meal for the whole{" "}
+              <span
+                style={{
+                  color: "#83B841",
+                  fontFamily: "monospace",
+                }}
+              >
+                family
+              </span>
+            </Typography>
+          </Box>
+          <Box component="div" className="banner2mobile">
+            <Typography
+              variant="h2"
+              textAlign="center"
+              sx={{
+                fontWeight: 700,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                height: "100%",
+                // zIndex: 1,
+                color: "#fff",
+                fontSize: 40,
+              }}
+            >
+              Amazing
+              <span
+                style={{
+                  color: "#83B841",
+                  fontFamily: "monospace",
+                }}
+              >
+                recipies
+              </span>
+              for you to try
+            </Typography>
+          </Box>
+          <Box component="div" className="banner3mobile">
+            <Typography
+              variant="h2"
+              textAlign="center"
+              sx={{
+                fontWeight: 700,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                height: "100%",
+                // zIndex: 1,
+                color: "#fff",
+                fontSize: 40,
+              }}
+            >
+              Freshly made with
+              <span
+                style={{
+                  color: "#698F3C",
+                  fontFamily: "monospace",
+                }}
+              >
+                the best
+              </span>
+              ingredients & love
+            </Typography>
+          </Box>
+        </Carousel>
       </Box>
-      <Box component="div" className="banner2">
-        <Typography
-          variant="h2"
-          textAlign="center"
-          sx={{
-            fontWeight: 700,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            textAlign: "center",
-            height: "100%",
-            color: "#fff",
-            fontSize: { md: 80, sm: 60, xs: 55 },
-          }}
-        >
-          Perfect meal for the whole{" "}
-          <span
-            style={{
-              color: "#83B841",
-              fontFamily: "monospace",
-            }}
-          >
-            Family
-          </span>
-        </Typography>
-      </Box>
-      <Box component="div" className="banner3">
-        <Typography
-          variant="h2"
-          textAlign="center"
-          sx={{
-            fontWeight: 700,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            textAlign: "center",
-            height: "100%",
-            color: "#fff",
-            fontSize: { md: 80, sm: 60, xs: 55 },
-          }}
-        >
-          Freshly sourced from local{" "}
-          <span
-            style={{
-              color: "#698F3C",
-              fontFamily: "monospace",
-            }}
-          >
-            Farmers
-          </span>
-        </Typography>
-      </Box>
-    </Carousel>
+    </>
   );
 }
